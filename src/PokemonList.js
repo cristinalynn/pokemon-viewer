46ea33f7315ae423714a4d47fc822506a35f2faf
Cristina Lynn <cristina@cristinalynn.net>
2023-07-14 13:12:17 -0700

    Move pokemon.map from Home into PokemonList component

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,5 +1,14 @@
 function BlogList() {
     return (
+        <div className="pokemon-list">
+            {pokemon.map((pokemon) => (
+            <div className="pokemon-preview" key={pokemon.id}>
+               <h2>{ pokemon.name }</h2>
+               <p>Type: { pokemon.type }</p>
+               <p>HP: { pokemon.hp }</p>
+            </div>
+          ))}
+        </div>
         
     );
 }
