e58ddb2a6e368dd1d63ba5d8885ccce7c203913b
Cristina Lynn <cristina@cristinalynn.net>
2023-07-14 09:25:58 -0700

    Add state in Home component

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
-function Home() {
+import { useState } from "react";
 
-    function handleClick() {
-        
-    }
+function Home() {
+    const [pokemon, setPokemon] = useState([
+        {name: "Pickachu", type: "electric", hp: 60, id:1},
+        {name: "Eevee", type: "normal", hp: 70, id:2},
+        {name: "Charmander", type: "fire", hp: 70, id:3}
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
+           {pokemon.map((pokemon) => (
+             <div className="pokemon-preview" key={pokemon.id}>
+                <h2>{ pokemon.name }</h2>
+                <p>Type: { pokemon.type }</p>
+             </div>
+           ))}
         </div>
     );
 }
