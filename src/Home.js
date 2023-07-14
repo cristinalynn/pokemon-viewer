e3ccaa212ddc2230d97b874b80a9111f9f4fce30
Cristina Lynn <cristina@cristinalynn.net>
2023-07-14 14:36:56 -0700

    Fetch the data to the dom

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,22 @@ import { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
 
 function Home() {
-    const [pokemon, setPokemon] = useState([
-        {name: "Pickachu", type: "electric", hp: 60, id:1},
-        {name: "Eevee", type: "normal", hp: 70, id:2},
-        {name: "Charmander", type: "fire", hp: 70, id:3}
-    ]);
+    const [pokemon, setPokemon] = useState(null);
+       
 
     useEffect(() => {
-        console.log("use effect ran")
+        fetch("http://localhost:8000/pokemon")
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setPokemon(data);
+        })
     }, []);
 
     return (
         <div className="home">
-          <PokemonList  pokemon={pokemon} title="All Pokemon!"/>
+          {pokemon && <PokemonList  pokemon={pokemon} title="All Pokemon!"/>}
         </div>
     );
 }
