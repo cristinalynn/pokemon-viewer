a0c7d3137237a7a11136f1c22dd54712e2b6b902
Cristina Lynn <cristina@cristinalynn.net>
2023-07-14 08:21:20 -0700

    Add className links to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             <h1>Pokemon World</h1>
+            <div className="links">
+                <a href="/"></a>
+            </div>
         </nav>
     );
 }
