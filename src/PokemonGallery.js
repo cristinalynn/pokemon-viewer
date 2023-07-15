fb24c628e5586713271944da5b580af69fea3486
Cristina Lynn <cristina@cristinalynn.net>
2023-07-15 12:44:57 -0700

    Create PokemonGallery component

---
+++ src/PokemonGallery.js
@@ -0,0 +1,10 @@
+
+function PokemonGallery() {
+    return (
+        <div className="pokemon-gallery">
+            <h2>Pokemon gallery</h2>
+        </div>
+    );
+}
+ 
+export default PokemonGallery;
