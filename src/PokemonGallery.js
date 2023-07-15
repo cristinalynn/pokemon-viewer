9b42725ab37b75320b28a2a3bb76f91037da72e6
Cristina Lynn <cristina@cristinalynn.net>
2023-07-15 12:50:10 -0700

    Import useParams in PokemonGallery component

--- src/PokemonGallery.js
+++ src/PokemonGallery.js
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom";
 
 function PokemonGallery() {
+    const { id } = useParams();
+
     return (
         <div className="pokemon-gallery">
-            <h2>Pokemon gallery</h2>
+            <h2>Pokemon gallery - { id }</h2>
         </div>
     );
 }
