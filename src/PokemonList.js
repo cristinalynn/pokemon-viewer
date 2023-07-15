65a8faa2492e5c070365c2e9c201ef6b96b5bd51
Cristina Lynn <cristina@cristinalynn.net>
2023-07-15 12:58:04 -0700

    Add Link component to PokemonList component

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function PokemonList({ pokemon, title }) {
     
     return (
@@ -5,9 +7,11 @@ function PokemonList({ pokemon, title }) {
             <h2>{ title }</h2>
             {pokemon.map((pokemon) => (
             <div className="pokemon-preview" key={pokemon.id}>
-               <h2>{ pokemon.name }</h2>
-               <p>Type: { pokemon.type }</p>
-               <p>HP: { pokemon.hp }</p>
+                <Link to={`/pokemon/${pokemon.id}`}> 
+                    <h2>{ pokemon.name }</h2>
+                    <p>Type: { pokemon.type }</p>
+                    <p>HP: { pokemon.hp }</p>
+               </Link>
             </div>
           ))}
         </div>
