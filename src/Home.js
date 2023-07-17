046391db2484599b2672791ad4be4f1a9f3b6eb4
Cristina Lynn <cristina@cristinalynn.net>
2023-07-17 14:47:18 -0700

    Update Home component

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,14 @@
-import { useState, useEffect } from "react";
+
 import PokemonList from "./PokemonList";
 
-function Home() {
-    const [pokemon, setPokemon] = useState(null);
 
-
-
-    useEffect(() => {
-        fetch("http://localhost:8000/pokemon")
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setPokemon(data);
-        })
-    }, []);
+function Home( {pokemon}) {
+   
 
     return (
         <div className="home">
-          {pokemon && <PokemonList  pokemon={pokemon} title="All Pokemon!"/>}
+           <PokemonList  pokemon={pokemon} title="All Pokemon!"/>
+         
         </div>
     );
 }
