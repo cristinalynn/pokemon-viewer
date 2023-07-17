6a0c17d244db0dd62e88f50b8afa581c90829bea
Cristina Lynn <cristina@cristinalynn.net>
2023-07-17 14:41:27 -0700

    Set state and effect in App component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,45 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
+import { useState, useEffect } from "react";
 import PokemonGallery from "./PokemonGallery";
 
+
+
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+      fetch("http://localhost:8000/pokemon")
+      .then(res => {
+          return res.json();
+      })
+      .then(data => {
+          setPokemon(data);
+      })
+  }, []);
+  
   return (
+  
     <Router>
       <div className="App">
         <Navbar />
         <div className="content">
           <Switch>
             <Route exact path="/">
-                <Home />
+                <Home pokemon={pokemon}/>
             </Route>
             <Route path="/create">
                 <Create />
             </Route>
-            <Route path="/pokemon/:id">
-                <PokemonGallery />
-            </Route>
+            <Route path="/pokemonGallery">
+                <PokemonGallery pokemon={pokemon}/>
+            </Route >
           </Switch>
         </div>
       </div>
     </Router>
+    
   );
 }
 
